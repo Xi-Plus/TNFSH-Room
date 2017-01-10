512b6ab8eecc9a01700cdd0b87f43dc028e0b762
xiplus <huangxuanyuxiplus@gmail.com>
2017-01-10 11:50:28 +0800

    home show log time ago

--- func/common.php
+++ func/common.php
@@ -9,4 +9,19 @@ function het($text){
 function getrandommd5(){
 	return md5(uniqid(rand(),true));
 }
+function timelen($sec){
+	if ($sec<60) {
+		return $sec."秒";
+	} else if ($sec<60*60) {
+		return floor($sec/60)."分";
+	} else if ($sec<60*60*24) {
+		return floor($sec/(60*60))."時";
+	} else if ($sec<60*60*24*31) {
+		return floor($sec/(60*60*24))."日";
+	} else if ($sec<60*60*24*365) {
+		return floor($sec/(60*60*24*30))."月";
+	} else {
+		return floor($sec/(60*60*24*365))."年";
+	}
+}
 ?>
